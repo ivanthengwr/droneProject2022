dafe63f2b3bc2a28793038cdf4c205b714aef08f
Ivan Theng <ivanthengwr@gmail.com>
2022-07-27 11:47:13 +0800

    added in define values

--- drone_project/motor-ESC-battery_testing/motor-ESC-battery_testing.ino
+++ drone_project/motor-ESC-battery_testing/motor-ESC-battery_testing.ino
@@ -4,6 +4,9 @@ Servo myservo2;
 Servo myservo3;
 Servo myservo4;
 
+#define arm 15
+#define disarm 20
+
 bool armed =  false;
 
 int ccwMotor1 = 6;
@@ -20,10 +23,10 @@ void setup() {
   myservo3.attach(cwMotor3);
   myservo4.attach(cwMotor4);
   delay(1);
-  myservo1.write(15); //Required to activate ESC
-  myservo2.write(15);
-  myservo3.write(15);  
-  myservo4.write(15);
+  myservo1.write(arm); //Required to activate ESC
+  myservo2.write(arm);
+  myservo3.write(arm);  
+  myservo4.write(arm);
   
  // delay(4000); //Mandatory Wait Time
 }
@@ -39,16 +42,16 @@ void loop() {
   int val = 0;
   while(armed){
     val = 49;
-    Serial.println(val);   //Displays read int value
+    Serial.println(val);    //Displays read int value
     myservo1.write(val);    // Writes read int value to ESC
     myservo2.write(val);
     myservo3.write(val); 
     myservo4.write(val);
     delay(2000);
     armed = false;
-    myservo1.write(20);
-    myservo2.write(20);
-    myservo3.write(20); 
-    myservo4.write(20);
+    myservo1.write(disarm);
+    myservo2.write(disarm);
+    myservo3.write(disarm); 
+    myservo4.write(disarm);
   }
 }
